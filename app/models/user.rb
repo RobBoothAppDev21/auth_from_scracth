3aee46c62feff4693803ec02c70f0595d9481473
Rob Young <rhyoung214@gmail.com>
2024-01-13 13:00:29 -0800

    completed simple authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
   normalizes :email, with: ->(email) { email.strip.downcase }
+
+  generates_token_for :password_reset, expires_in: 2.hours do
+    password_salt&.last(10)
+  end
+
+  # generates_token_for :email_confirmation, expires_in: 24.hours do
+  #   email
+  # end
 end
